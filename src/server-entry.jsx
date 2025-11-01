5236a3ff7b621acc909a3bb7b891a9a14ebac95e
wilson <wilsonesmabe2003@gmail.com>
2025-11-01 14:51:45 +0800

    initial template for react

---
+++ src/server-entry.jsx
@@ -0,0 +1,14 @@
+import ReactDOMServer from 'react-dom/server'
+import { StaticRouter, Routes, Route } from 'react-router'
+
+import App from './App'
+
+export function render(url, context) {
+  return ReactDOMServer.renderToString(
+    <StaticRouter location={url} context={context}>
+      <Routes>
+        <Route path='/' element={<App />} />
+      </Routes>
+    </StaticRouter>
+  )
+}
